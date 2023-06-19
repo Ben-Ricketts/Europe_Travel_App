9b60836117c42088e238a394639061516ac74712
Benske4 <128804040+Benske4@users.noreply.github.com>
2023-06-19 21:54:08 +1200

    list items change colour

--- starter/script.js
+++ starter/script.js
@@ -3,14 +3,25 @@ let ul = document.querySelector(".itemsAdded");
 let enterButton = document.getElementById("enter");
 let item = document.getElementsByTagName("li");
 
+function inputLength() {
+  return input.value.length;
+}
 function createListElement() {
   let li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
   input.value = "";
-
-  //   let deleteButton = document.createElement("button");
-  //   deleteButton.appendChild(document.createTextNode("X"));
 }
 
-enterButton.addEventListener("click", createListElement);
+function addListAfterClick() {
+  if (inputLength() > 0) createListElement();
+}
+
+function selectListItem() {
+  //   let itemsAdded = document.querySelector("li");
+  let listItem = event.target;
+  listItem.style.backgroundColor = "#90ee90";
+}
+
+enterButton.addEventListener("click", addListAfterClick);
+ul.addEventListener("click", selectListItem);
